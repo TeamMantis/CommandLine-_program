9868815edffc3baf833a5acfcd46228def75cb2c
myrdstom <nserekopaul@gmail.com>
2018-06-01 11:52:33 +0300

    [Finishes #158023121] Feature: Slight improvements to the login page

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
 import click
 
 @click.command()
-def hello():
+def login():
     click.echo(input('Insert Role:'))
     click.echo(input('Insert Username:'))
     click.echo(input('Insert Password:'))
     click.echo('You have been succesfully logged in!!!')
 if __name__ == '__main__':
-    hello()
+    login()

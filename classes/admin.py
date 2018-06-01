119379c8ab426af253f28c3cc10fb3d78f48df2f
Ayeeta <elijate@gmail.com>
2018-06-01 11:15:14 +0300

    [Chore #158024674] Sets up project files and folders

---
+++ classes/admin.py
@@ -0,0 +1,19 @@
+class Admin(object):
+    def __init__(self):
+        users = {}
+    def login(self, user_name, password, type):
+        pass
+    
+    def create_comment(self, comment, date):
+        pass
+    
+    def edit_comment(self, comment, new_comment):
+        pass
+
+    def delete_comment(self):
+        pass
+
+
+
+
+
